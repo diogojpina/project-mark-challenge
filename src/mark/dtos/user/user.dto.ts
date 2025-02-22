aaa0ae519558aecc44a884ebe46e2116fcdc21c0
Diogo Pina <diogojpina@gmail.com>
2025-02-22 10:07:55 -0300

    Fix user dto import

--- src/mark/dtos/user/user.dto.ts
+++ src/mark/dtos/user/user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { UserRoleEnum } from 'src/mark/enum/user.role.enum';
 
 export class UserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsEnum(UserRoleEnum)
   role: UserRoleEnum;
 }
